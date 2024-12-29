f3c1a9d3b6516f7990d0373411ad7e78e130164d
Rohi Modak <rohi.modak@localhost>
2024-12-29 21:58:51 +0530

    Document Selection API and related changes

--- rag_qa/rag_qa/core/api/views.py
+++ rag_qa/rag_qa/core/api/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import time
+from django.db import transaction
 
 from rest_framework import status
 from rest_framework.permissions import AllowAny
@@ -6,8 +8,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from rag_qa.core.models import Document
-from rag_qa.core.api.serializers import DocumentSerializer
-
+from rag_qa.core.api.serializers import DocumentSelectionSerializer, DocumentSerializer
+from rag_qa.core.tasks import build_index
 
 class DocumentIngestView(APIView):
     permission_classes = [AllowAny]
@@ -19,10 +21,36 @@ class DocumentIngestView(APIView):
             name = serializer.validated_data["name"]
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        Document.objects.create(file_path=file_path, name=name)
+        document = self._save_document(file_path, name)
+        self._process_document(document)
 
         return Response(
             {"message": "Document ingested successfully", "file_path": file_path},
             status=status.HTTP_201_CREATED,
         )
+    
+    def _save_document(self, file_path, name):
+        document = Document(file_path=file_path, name=name)
+        document.save()
+        return document
+
+    def _process_document(self, document):
+        document_id = document.id
+        transaction.on_commit(func=lambda: build_index.apply_async(args=[document_id]))
+
+    
+class DocumentSelectionView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        documents = Document.objects.all()
+        serializer = DocumentSelectionSerializer(documents, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        serializer = DocumentSelectionSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            for details in serializer.validated_data:
+                document = Document.objects.filter(id=details["id"]).update(selected=details["selected"])
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
